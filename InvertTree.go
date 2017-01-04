a67d5397574d44170580835563f1a15d12b5be80
baorongquan <brq0122@163.com>
2017-01-04 14:45:43 +0800

    resolve LeetCode 226 Invert Binary Tree

---
+++ InvertTree.go
@@ -0,0 +1,50 @@
+// LeetCode 226. Invert Binary Tree
+/*Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	root := &TreeNode{4, &TreeNode{2, &TreeNode{1, nil, nil}, &TreeNode{3, nil, nil}}, &TreeNode{7, &TreeNode{6, nil, nil}, &TreeNode{9, nil, nil}}}
+	printTree(root)
+	fmt.Println()
+	invertTree(root)
+	printTree(root)
+	fmt.Println()
+}
+
+func printTree(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	fmt.Printf("%d ", root.Val)
+	printTree(root.Left)
+	printTree(root.Right)
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
+	return root
+}
